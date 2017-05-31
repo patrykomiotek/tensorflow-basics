4323c8b013c823a0d835e57cf976e0c73d3e64e1
Patryk Omiotek <patryk.omiotek@gmail.com>
2017-05-31 16:45:28 +0200

    linear regression visualized with tensorboard

--- 09-linear-tensorboard.py
+++ 09-linear-tensorboard.py
@@ -1,16 +1,17 @@
 import tensorflow as tf
 
 # Model parameters
-W = tf.Variable([.3], tf.float32)
-b = tf.Variable([-.3], tf.float32)
+W = tf.Variable([.3], tf.float32, name='W')
+b = tf.Variable([-.3], tf.float32, name='b')
 
 # Model input and output
-x = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32, name='x-input')
 linear_model = W * x + b
-y = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32, name='y-input')
 
 # loss
 loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+tf.summary.scalar("loss", loss)
 
 # optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01)
@@ -24,8 +25,14 @@ y_train = [0, -1, -2, -3]
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init) # reset values to wrong
-for i in range(1000):
-  sess.run(train, {x: x_train, y: y_train})
+
+# Merge all summaries into a single operator
+summary_op = tf.summary.merge_all()
+summary_writer = tf.summary.FileWriter('/tmp/tensorflow/pom-train', graph=tf.get_default_graph())
+
+for step in range(1000):
+  _, summary = sess.run([train, summary_op], {x: x_train, y: y_train})
+  summary_writer.add_summary(summary, step)
 
 # evaluate training accuracy
 curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x: x_train, y: y_train})
